8fcbc4de78ae55733b60f02a9fe14b807bd9c664
Halilova Irina <irina.halilova@progforce.com>
2022-06-27 10:55:23 +0300

    database

--- express.js
+++ express.js
@@ -1,37 +1,31 @@
 import express from 'express'
 import path from 'path'
 import fs from "fs";
+import pgp from "pg-promise"
 
-const __dirname=path.resolve()
+const __dirname = path.resolve()
 const app = express()
 const PORT = process.env.PORT ?? 3000
 
+const test = pgp(/*options*/)
+const db = test("postgres://irina:topsecret@localhost:5432/testdb")
+
+db.one("SELECT $1 AS value", 123)
+    .then(function (data) {
+        console.log("DATA:", data.value);
+    })
+    .catch(function (error) {
+        console.log("ERROR:", error);
+    });
+
+
 app.use(express.static(path.join(__dirname, "static")));
 
-app.get("/", function(req, res){
-    
-         // response.send("<h1>Главная страницаfffff</h1>");
-        res.sendFile(path.resolve(__dirname,'static','index.html'))
- });
+app.get("/", function (req, res) {
+    res.sendFile(path.resolve(__dirname, 'static', 'index.html'))
+});
 
- 
-app.listen(PORT,()=>{
+
+app.listen(PORT, () => {
     console.log(`Server has been started on port ${PORT}`)
 })
-
-// const express = require("express");
- 
-// const app = express();
-// app.get("/", function(request, response){
-     
-//     response.send("<h1>Главная страница</h1>");
-// });
-// app.get("/about", function(request, response){
-     
-//     response.send("<h1>О сайте</h1>");
-// });
-// app.get("/contact", function(request, response){
-     
-//     response.send("<h1>Контакты</h1>");
-// });
-// app.listen(3000);
